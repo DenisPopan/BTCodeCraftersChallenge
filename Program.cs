d0b438985c016b3b6a1d2196fd08ee455f25edb7
Denis Popan <popandenis2013@gmail.com>
2023-07-10 01:07:24 +0300

    Renamed constant

--- Program.cs
+++ Program.cs
@@ -6,13 +6,14 @@
         {
             long userId = 42;
             DateTime dateTime = DateTime.Now;
+            const int ValidityPeriodSeconds = 30;
             TOTPSystem totpGenerator = new TOTPSystem();
 
             string otp = totpGenerator.GenerateTOTP(userId, dateTime);
             bool isTOTPValid = totpGenerator.ValidateOTP(otp);
             Console.WriteLine($"TOTP validity: {isTOTPValid}");
             Console.WriteLine("TOTP will expire soon...");
-            Thread.Sleep(1000 * 30);
+            Thread.Sleep(1000 * ValidityPeriodSeconds);
             Console.WriteLine($"TOTP validity: {totpGenerator.ValidateOTP(otp)}");
         }
 
